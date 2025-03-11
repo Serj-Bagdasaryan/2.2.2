b8c649fee67971bda6a40e47c3c7f0de6366960f
Serj Bagdasaryan <3402370@gmail.com>
2025-03-11 21:46:44 +0300

    Resolved with Spring.

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,40 +1,22 @@
 package web.controller;
 
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.config.WebConfig;
 import web.model.Car;
 import web.service.CarService;
-import web.service.CarServiceImpl;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class CarController {
+    @Autowired
+    private CarService carService;
 
     @GetMapping("/cars")
-    public String carController(@RequestParam(value = "count", defaultValue = "5") int count, ModelMap model) {
-        AnnotationConfigApplicationContext context = null;
-        try {
-            context = new AnnotationConfigApplicationContext(WebConfig.class);
-
-            CarService carService = context.getBean("carService", CarService.class);
-
-
-            List<Car> cars = carService.getCars(count);
-            model.addAttribute("cars", cars);
-            return "car";
-        } finally {
-            context.close();
-        }
+    public String showCars(@RequestParam(value = "count", defaultValue = "5") int count, ModelMap model) {
+        model.addAttribute("cars", carService.carService(count));
+        return "car";
     }
-
-
-
 }
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,29 +1,30 @@
 package web.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import javax.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
 import web.model.Car;
-
-import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class CarServiceImpl implements CarService {
-    private List<Car> cars;
+    private static List<Car> cars;
+
+    public List<Car> carService(Integer count) {
+        if (count == null || count >= 5) {
+            return cars;
+        } else {
+            return cars.subList(0, count);
+        }
+    }
 
     @PostConstruct
     private void initCarList() {
-        cars = new ArrayList<Car>();
+        cars = new ArrayList<>();
         cars.add(new Car("First", 1111, 111));
         cars.add(new Car("Second", 2222, 222));
         cars.add(new Car("Third", 3333, 333));
         cars.add(new Car("Fourth", 4444, 444));
         cars.add(new Car("Fifth", 5555, 555));
-    }
-
-    public List<Car> getCars(int count) {
-        return cars.subList(0, count);
     }
 }
